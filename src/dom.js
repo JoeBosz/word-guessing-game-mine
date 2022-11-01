253ed02f85fac8978515e2fe2d468492ea9eca17
JoeBosz <joeboz2213@gmail.com>
2022-10-31 20:35:09 -0500

    Add all dom querys

---
+++ src/dom.js
@@ -0,0 +1,7 @@
+export const startBtn = document.querySelector("header button");
+export const lossesP = document.getElementById("losses");
+export const resetBtn = document.querySelector("main button");
+export const seconds = document.getElementById("seconds");
+export const startBtn = document.querySelector("header button");
+export const winsP = document.getElementById("wins");
+export const wordP = document.getElementById("word");
